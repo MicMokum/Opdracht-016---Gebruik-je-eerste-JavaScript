c8bb6446256ed7e8ffa2b7e7631abbdc10cdd4a0
MicMokum <80250147+MicMokum@users.noreply.github.com>
2021-03-23 21:55:45 +0100

    Add files via upload

---
+++ Opdracht 016 - Gebruik je eerste JavaScript/script.js
@@ -0,0 +1,26 @@
+console.log("Hello Winc Academy");
+let myName = "Michiel";
+console.log(myName);
+// let if = "Mag niet want ingebouwd java woord"
+let som = 4 + 4;
+console.log(som);
+let stringSom = "4" + "4"; // geen foutmelding dus mag wel
+console.log(stringSom);  // strings worden keurig aan elkaar geplakt
+let optellen = 1 + 1;
+let aftrekken = 1 - 1;
+let vermenigvuldigen = 1 * 1;
+let delen = 1 / 1;
+let geenIdeeWatDitDoet = 1 % 1;
+let zouDitHetZijn = 11 % 3;  // wat overblijft als je deelt, in dit geval rest 2
+console.log(optellen);
+console.log(aftrekken);
+console.log(vermenigvuldigen);
+console.log(delen);
+console.log(geenIdeeWatDitDoet);
+console.log(zouDitHetZijn);
+let myAge = 18.5;
+let myAgeString = "18.5";
+console.log(myAge);
+console.log(myAgeString);
+console.log(typeof(myAge));
+console.log(typeof(myAgeString));
